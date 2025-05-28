e47ccef7671216e76679297ab1fbe2ac33180055
kagirimichael <michaelkagiri6@gmail.com>
2025-05-28 15:25:54 +0300

    types

--- typescript.ts
+++ typescript.ts
@@ -18,3 +18,22 @@ let hasPermission : boolean = false;
 //array
 let numbers : number[] = [1,2,3];
 let names : string[] = ["joe", "marc", "onana"];
+
+
+//any
+//it bypass type checking for variables
+
+let types : any = [1,2,3];
+types = "hello there";
+types = false;
+
+//void used for functios that return no value
+function sayHello() : void {
+    console.log("hello");
+    
+};
+
+// null & undefined These are distinct types in TypeScript, just like in JavaScript. They represent the absence of a value.
+let n = null;
+let u = undefined;
+
