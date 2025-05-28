8624df75447b0fb7f0862e7d772f4087c79302e7
kagirimichael <michaelkagiri6@gmail.com>
2025-05-28 18:09:02 +0300

    types

---
+++ alias.js
@@ -0,0 +1,4 @@
+var id1 = "user9876543";
+var id2 = 76543234567;
+var meet = { x: 2, y: 3 };
+var Active = "Active";
---
+++ alias.ts
@@ -0,0 +1,10 @@
+type ID = string | number
+type point = {x:number, y:number}
+type status = "Active" | "inActive" | "pending"
+
+let id1 : ID = "user9876543"
+let id2 : ID = 76543234567
+
+let meet : point = {x:2, y:3};
+
+let Active : status = "Active"
