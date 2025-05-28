45a1db65c613e73c2ddea015a7a21cf8933463e7
kagirimichael <michaelkagiri6@gmail.com>
2025-05-28 18:40:22 +0300

    types

---
+++ enums.js
@@ -0,0 +1,18 @@
+var Direction;
+(function (Direction) {
+    Direction[Direction["up"] = 0] = "up";
+    Direction[Direction["down"] = 1] = "down";
+    Direction[Direction["left"] = 2] = "left";
+    Direction[Direction["right"] = 3] = "right";
+})(Direction || (Direction = {}));
+var playerDirection = Direction.right;
+console.log(playerDirection);
+var Colors;
+(function (Colors) {
+    Colors["red"] = "RED";
+    Colors["blue"] = "BLUE";
+    Colors["grey"] = "GREY";
+    Colors["pink"] = "PINK";
+})(Colors || (Colors = {}));
+var favourite = Colors.red;
+console.log(favourite);
---
+++ enums.ts
@@ -0,0 +1,17 @@
+enum Direction {
+    up,
+    down,
+    left,
+    right
+}
+let playerDirection : Direction = Direction.right;
+console.log(playerDirection);
+
+enum Colors {
+    red = "RED",
+    blue = "BLUE",
+    grey = "GREY",
+    pink = "PINK"
+}
+let favourite : Colors = Colors.red;
+console.log(favourite);
